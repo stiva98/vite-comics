0a7deb8fc70bba1c5debd653bcec949563c48cab
stiva98 <samustiva9810@gmail.com>
2023-07-14 17:19:00 +0200

    ADD ciclo e i props

---
+++ src/components/FilmComponent.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    name: "FilmComponent",
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        filmImage: String,
+        filmTitle: String
+    }
+}
+</script>
+<template>
+    <div class="card">
+        <img :src="filmImage" :alt="filmTitle">
+        <h3>
+            {{ filmTitle }}
+        </h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+@use "../assets/scss/film.scss" as *;
+</style>
